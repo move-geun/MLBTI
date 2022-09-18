52ddbfc3fc9ad87622e8dee5b1ee0bc4c3c3b03c
이동근 <ehdrms1426@gmail.com>
2022-09-18 22:02:27 +0900

    add:커스텀시뮬레이션페이지

--- frontend/src/pages/simulation/CustomSimulationPage.style.js
+++ frontend/src/pages/simulation/CustomSimulationPage.style.js
@@ -3,11 +3,21 @@ import styled from "styled-components";
 const CustomConatiner = styled.div`
   display: flex;
   height: 100%;
-  width: 100%;
+  width: 800px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin: 0 auto;
   font-size: 1.5rem;
+
+  @media screen and (max-width: 830px) {
+    font-size: 1.3rem;
+    width: 600px;
+  }
+  @media screen and (max-width: 480px) {
+    font-size: 1rem;
+    width: 400px;
+  }
 
   .start {
     display: flex;
@@ -44,45 +54,75 @@ const TeamCase = styled.div`
 `;
 const ModalBox = styled.div`
   position: absolute;
-  top: 30%;
+  top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   width: 330px;
-  height: 200px;
+  height: 500px;
   border: 2px solid rgba(0, 0, 0, 0.2);
   background-color: white;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: flex-start;
   border-radius: 5px;
 
   .title {
+    margin-top: 10px;
     width: 90%;
     text-align: center;
     font-size: 1.5rem;
   }
 
   .content {
-    width: 95%;
+    width: 90%;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    font-size: 1rem;
-
-    input {
-      width: 50%;
-      border-bottom: 1px solid black;
-    }
-    button {
-      padding: 0.2rem 0.4rem;
-    }
+    font-size: 1.3rem;
   }
 
+  .weather {
+    display: flex;
+    font-size: 1rem;
+  }
+  .team {
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    font-size: 1.3rem;
+  }
+  .filter {
+    display: flex;
+    flex-direction: row;
+    margin-top: 5px;
+  }
+  .candidates {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin: 5px 0;
+
+    .candi {
+      margin: 5px 0;
+    }
+  }
   .change {
     font-size: 1rem;
     padding: 0.3rem 0.5rem;
+  }
+
+  input {
+    width: 50%;
+    border-bottom: 1px solid black;
+    margin-right: 10px;
+  }
+  button {
+    font-size: 1rem;
+    padding: 0.2rem 0.4rem;
   }
 `;
 export { CustomConatiner, TeamContainer, TeamCase, ModalBox };
