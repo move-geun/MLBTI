41630e3f9e13c4c9e51ba5b94f6261c9cd0fee63
배송윤 <nename8@naver.com>
2022-09-19 14:46:34 +0900

    add: 팀 커스텀 전력 테이블 생성

--- frontend/src/components/TeamCustom/TeamComposition.jsx
+++ frontend/src/components/TeamCustom/TeamComposition.jsx
@@ -6,10 +6,35 @@ import {
   ListWrapper,
   MyNickname,
   ListHeader,
+  ListTable,
+  ListBody,
   HeaderDetail,
   PlyaerList,
   PlayerDetail,
 } from "./TeamCoposiotion.style";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(position, name, team, stats) {
+  return { position, name, team, stats };
+}
+
+const playerList = [
+  createData("투수", "이동근", "남포동", 99),
+  createData("투수", "이동근", "남포동", 99),
+  createData("투수", "이동근", "남포동", 99),
+  createData("투수", "이동근", "남포동", 99),
+];
+
+const TaBleList = styled(TableCell)`
+  font-family: "MICEGothic Bold";
+`;
 
 const TeamCoposition = () => {
   return (
@@ -18,39 +43,33 @@ const TeamCoposition = () => {
         <MyNickname>홈런맞아부러쓰 </MyNickname>
         <p>의 전력</p>
       </Header>
-      <ListWrapper>
-        <ListHeader className="Header">
-          <HeaderDetail>포지션</HeaderDetail>
-          <HeaderDetail>선수명</HeaderDetail>
-          <HeaderDetail>소속팀</HeaderDetail>
-          <HeaderDetail>선수스탯</HeaderDetail>
-        </ListHeader>
-
-        <PlyaerList>
-          <PlyaerDetail>투수</PlyaerDetail>
-          <PlyaerDetail>Aldfs adflkdsf</PlyaerDetail>
-          <PlyaerDetail>LA다져스</PlyaerDetail>
-          <PlyaerDetail>0.302</PlyaerDetail>
-        </PlyaerList>
-        <PlyaerList>
-          <PlyaerDetail>투수</PlyaerDetail>
-          <PlyaerDetail>Aldfs adflkdsf</PlyaerDetail>
-          <PlyaerDetail>LA다져스</PlyaerDetail>
-          <PlyaerDetail>0.302</PlyaerDetail>
-        </PlyaerList>
-        <PlyaerList>
-          <PlyaerDetail>투수</PlyaerDetail>
-          <PlyaerDetail>Aldfs adflkdsf</PlyaerDetail>
-          <PlyaerDetail>LA다져스</PlyaerDetail>
-          <PlyaerDetail>0.302</PlyaerDetail>
-        </PlyaerList>
-        <PlyaerList>
-          <PlyaerDetail>투수</PlyaerDetail>
-          <PlyaerDetail>Aldfs adflkdsf</PlyaerDetail>
-          <PlyaerDetail>LA다져스</PlyaerDetail>
-          <PlyaerDetail>0.302</PlyaerDetail>
-        </PlyaerList>
-      </ListWrapper>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TaBleList>포지션</TaBleList>
+              <TaBleList align="right">선수명</TaBleList>
+              <TaBleList align="right">소속팀</TaBleList>
+              <TaBleList align="right">선수 스탯</TaBleList>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {playerList.map((player) => (
+              <TableRow
+                key={player.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TaBleList component="th" scope="player">
+                  {player.position}
+                </TaBleList>
+                <TaBleList align="right">{player.name}</TaBleList>
+                <TaBleList align="right">{player.team}</TaBleList>
+                <TaBleList align="right">{player.stats}</TaBleList>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </CompositionWrapper>
   );
 };
