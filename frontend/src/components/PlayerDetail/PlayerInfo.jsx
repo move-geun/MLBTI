0c4477b26d66b320b9caa6221eff151d2af08c43
배송윤 <nename8@naver.com>
2022-09-20 20:06:15 +0900

    add: 선수 상세 페이지 구현 중

---
+++ frontend/src/components/PlayerDetail/PlayerInfo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlayerInfo = () => {
+
+  return (
+    <div></div>
+
+  )
+}
+
+export default PlayerInfo
---
+++ frontend/src/components/PlayerDetail/PlayerInfo.style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+
+const ImgDiv = styled.div`
+
+`
+
+const Name = styled.div`
+
+`
+
+const InfoDiv = styled.div`
+
+`
+
+const InfoTitle = styled.div`
+
+`
+
+const InfoContent = styled.div`
+
+`
+
