1d4a9ebb9840c60ed3826e799fb5a457bde9d995
배송윤 <nename8@naver.com>
2022-09-21 20:46:29 +0900

    add: 팀 상세 페이지 수정 중

--- frontend/src/components/TeamDetail/TeamInfo.jsx
+++ frontend/src/components/TeamDetail/TeamInfo.jsx
@@ -29,13 +29,49 @@ const TeamInfo = () => {
         <Info className="name">LA 다져스</Info>
         <Info className="rank">2위</Info>
         <Info className="detail">
-          <TableContainer sx={{ 
-            '@media screen and (max-width: 480px)': {
-              // width: '40%',
+          <table>
+            <thead >
+              <tr>
+                <th>승</th>
+                <th>패</th>
+                <th>승률</th>
+                <th>게임차</th>
+                <th>홈</th>
+                <th>원정</th>
+                <th>최근 10경기</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>79</td>
+                <td>51</td>
+                <td>.608</td>
+                <td>-</td>
+                <td>45-10</td>
+                <td>34-31</td>
+                <td>6-4</td>
+              </tr>
+            </tbody>
+
+          </table>
+        </Info>
+      </InfoWrapper>
+    </Wrapper>
+  );
+};
+
+export default TeamInfo;
+
+{
+  /* <TableContainer sx={{ 
+            '@media screen and (max-width: 510px)': {
+              width: '70%',
             },
             }}component={Paper}>
             <Table sx={{ 
-              }} 
+              witdh:'10px'  
+            }} 
+          
               aria-label="simple table">
               <TableHead>
                 <TableRow sx={{backgroundColor:'#7ce0ff'}}>
@@ -58,11 +94,5 @@ const TeamInfo = () => {
                 </TableRow>
               </TableBody>
             </Table>
-          </TableContainer>
-        </Info>
-      </InfoWrapper>
-    </Wrapper>
-  );
-};
-
-export default TeamInfo;
+          </TableContainer> */
+}
