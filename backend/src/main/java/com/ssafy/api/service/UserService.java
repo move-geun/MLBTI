ed4c86fd30465c673e2687b03b5d04bf3d295992
김동우 <sunbi5252@naver.com>
2022-09-21 17:35:27 +0900

    feat: 비밀번호 찾기, 정보 변경 기능 추가

--- backend/src/main/java/com/ssafy/api/service/UserService.java
+++ backend/src/main/java/com/ssafy/api/service/UserService.java
@@ -1,5 +1,7 @@
 package com.ssafy.api.service;
 
+import java.util.Optional;
+
 import com.ssafy.api.request.UserRegisterPostReq;
 import com.ssafy.db.entity.Users;
 
@@ -9,4 +11,6 @@ import com.ssafy.db.entity.Users;
 public interface UserService {
 	Users createUsers(UserRegisterPostReq userRegisterInfo);
 	Users getUsersByEmail(String userId);
+	void modifyUserInfo(String email, String newPassword,String nickName);
+	Optional<Users> getUsersByNickName(String nickname);
 }
--- backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
@@ -33,8 +33,6 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public Users createUsers(UserRegisterPostReq userRegisterInfo) {
 		Users user = new Users();
-		
-		
 		user.setEmail(userRegisterInfo.getEmail());
 		// 보안을 위해서 유저 패스워드 암호화 하여 디비에 저장.
 		user.setPassword(passwordEncoder.encode(userRegisterInfo.getPassword()));
@@ -43,10 +41,56 @@ public class UserServiceImpl implements UserService {
 		return userRepository.save(user);
 	}
 
+	/**
+	  * @Method Name : getUsersByEmail
+	  * @작성일 : 2022. 9. 21
+	  * @작성자 : 김동우
+	  * @변경이력 : 
+	
+	  * @Method 설명 : 이메일로 유저 객체를 가져옵니다.
+	  * @param Email
+	  * @return
+	  */
 	@Override
 	public Users getUsersByEmail(String Email) {
 		// 디비에 유저 정보 조회 (userId 를 통한 조회).
 		Users user = userRepositorySupport.findUserByEmail(Email).get();
 		return user;
 	}
+
+	/**
+	  * @Method Name : modifyUserPassword
+	  * @작성일 : 2022. 9. 21
+	  * @작성자 : 김동우
+	  * @변경이력 : 
+	
+	  * @Method 설명 :
+	  * @param email
+	  */
+	@Override
+	public void modifyUserInfo(String email,String newPassword, String newNickname) {
+		// TODO Auto-generated method stub
+		Users user = userRepositorySupport.findUserByEmail(email).get();
+
+		user.setNickname(newNickname);
+		user.setPassword(passwordEncoder.encode(newPassword));
+		userRepository.save(user);
+	}
+
+	/**
+	  * @Method Name : getUsersByNickName
+	  * @작성일 : 2022. 9. 21
+	  * @작성자 : 김동우
+	  * @변경이력 : 
+	
+	  * @Method 설명 : 닉네임으로 user 객체 가져온다.
+	  * @param nickname
+	  * @return
+	  */
+	@Override
+	public Optional<Users> getUsersByNickName(String nickname) {
+		return userRepository.findByNickname(nickname);
+	}
+	
+
 }
