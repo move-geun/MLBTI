ed469b28596207b6fd9c814fb44e8694df749b47
이동근 <ehdrms1426@gmail.com>
2022-09-21 14:17:15 +0900

    add : 경기기록페이지

--- frontend/src/pages/game/GameHistory.style.js
+++ frontend/src/pages/game/GameHistory.style.js
@@ -1,4 +1,3 @@
-import { style } from "@mui/system";
 import styled from "styled-components";
 
 const HistoryContainer = styled.div`
@@ -142,4 +141,58 @@ const ScoreInfo = styled.div`
     }
   }
 `;
-export { HistoryContainer, Score, ScoreTitle, ScoreInfo };
+
+const EventBox = styled.div`
+  width: 50%;
+  height: 150px;
+  margin: 3rem auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: gray;
+`;
+
+const TeamBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  @media screen and (max-width: 830px) {
+    font-size: 0.8rem;
+    margin-bottom: 3px;
+    flex-direction: column;
+  }
+  @media screen and (max-width: 480px) {
+    font-size: 0.625rem;
+    margin-bottom: 2px;
+  }
+`;
+
+const Team = styled.div`
+  width: 100%;
+  border-bottom: 2px solid black;
+  padding-bottom: 5px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  img {
+    width: 60px;
+    height: 60px;
+  }
+  .teamtitle {
+    margin-left: 1rem;
+    font-size: 2rem;
+  }
+`;
+
+export {
+  HistoryContainer,
+  Score,
+  ScoreTitle,
+  ScoreInfo,
+  EventBox,
+  TeamBox,
+  Team,
+};
