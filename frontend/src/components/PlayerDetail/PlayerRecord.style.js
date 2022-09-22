b3be625560a195a95392819ae77a3eeaa87840e2
김동우 <sunbi5252@naver.com>
2022-09-22 11:58:03 +0900

    fix: merge fix

--- frontend/src/components/PlayerDetail/PlayerRecord.style.js
+++ frontend/src/components/PlayerDetail/PlayerRecord.style.js
@@ -1,23 +1,17 @@
 import styled from "styled-components";
-<<<<<<< HEAD
 import TextField from "@mui/material/TextField";
-=======
->>>>>>> 01670c4 (add: 선수 상세 페이지 구현중)
 
 const Wrapper = styled.div`
   margin-top: 5rem;
   width: 90%;
-<<<<<<< HEAD
   background-color: #ceddf7;
   border-radius: 2rem;
-=======
->>>>>>> 01670c4 (add: 선수 상세 페이지 구현중)
 `;
 
 const Title = styled.div`
   display: flex;
   justify-content: center;
-  font-size: 60px;
+  font-size: 50px;
   margin-bottom: 2rem;
   @media screen and (max-width: 830px) {
     font-size: 40px;
@@ -29,16 +23,11 @@ const Title = styled.div`
 
 const Table = styled.table`
   width: 70%;
-<<<<<<< HEAD
-  margin: auto; 
-=======
   margin: auto;
->>>>>>> 01670c4 (add: 선수 상세 페이지 구현중)
   /* outline: 1px solid; */
   font-size: 30px;
 `;
 
-<<<<<<< HEAD
 const Header = styled.thead` 
   /* outline: 1px solid; */
   width: 40%;
@@ -48,15 +37,6 @@ const Header = styled.thead`
 const Td = styled.td`
   /* text-align: center; */
   padding: 1rem;
-=======
-const Header = styled.thead`
-  /* outline: 1px solid; */
-  width: 40%;
-  margin-bottom: 1rem;
-`;
-
-const Td = styled.td`
->>>>>>> 01670c4 (add: 선수 상세 페이지 구현중)
   @media screen and (max-width: 830px) {
     font-size: 20px;
   }
@@ -65,9 +45,6 @@ const Td = styled.td`
   }
 `;
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 01670c4 (add: 선수 상세 페이지 구현중)
 export { Wrapper, Title, Table, Header, Td };
