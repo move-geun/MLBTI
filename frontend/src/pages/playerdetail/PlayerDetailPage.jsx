6eb9c4b502ca79588b44b1302561c443e6fc9a44
김동우 <sunbi5252@naver.com>
2022-09-22 11:42:07 +0900

    fix: merge fix

--- frontend/src/pages/playerdetail/PlayerDetailPage.jsx
+++ frontend/src/pages/playerdetail/PlayerDetailPage.jsx
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
 import { Background, SearchDiv } from "./PlayerDetailPage.style";
 import PlayerInfo from "../../components/PlayerDetail/PlayerInfo";
 import PlayerRecord from "../../components/PlayerDetail/PlayerRecord";
 import { SearchInput, Img } from "../../pages/team/TeamCustomPage.style";
-=======
-import React from "react"
-import { 
-  Background, 
-} from "./PlayerDetailPage.style"
-import PlayerInfo from "../../components/PlayerDetail/PlayerInfo"
-import PlayerRecord from "../../components/PlayerDetail/PlayerRecord"
->>>>>>> 01670c4 (add: 선수 상세 페이지 구현중)
 
 const PlayerDetailPage = () => {
   return (
     <Background>
-<<<<<<< HEAD
       <SearchDiv>
         <div className="title">선수 프로필</div>
         <div className="search">
@@ -36,12 +26,5 @@ const PlayerDetailPage = () => {
     </Background>
   );
 };
-=======
-      <PlayerInfo/>
-      <PlayerRecord/>
-    </Background>
-    )
-} 
->>>>>>> 01670c4 (add: 선수 상세 페이지 구현중)
 
 export default PlayerDetailPage;
