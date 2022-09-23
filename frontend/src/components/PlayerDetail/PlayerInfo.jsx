a2ef874c891285d88cb1697652973ece099ccb8e
배송윤 <nename8@naver.com>
2022-09-23 09:11:48 +0900

    feat: 선수 디테일 css 수정

--- frontend/src/components/PlayerDetail/PlayerInfo.jsx
+++ frontend/src/components/PlayerDetail/PlayerInfo.jsx
@@ -15,37 +15,16 @@ import {
 const PlayerInfo = () => {
   return (
     <Wrapper>
-      <ProfileDiv>
-        <ImgDiv>
-          <ProfileImg src={"assets/chanho.jpg"} />
-        </ImgDiv>
-        <Name>박찬호</Name>
-      </ProfileDiv>
-
-      <InfoDiv>
-        <InfoBox>
-          <Content>
-            <Title>포지션</Title>
-            <Detail>우익수</Detail>
-          </Content>
-          <Content>
-            <Title>출생</Title>
-            <Detail>1999.03.02</Detail>
-          </Content>
-          <Content>
-            <Title>키</Title>
-            <Detail>188 (cm)</Detail>
-          </Content>
-          <Content>
-            <Title>체중</Title>
-            <Detail>99 (kg)</Detail>
-          </Content>
-          <Content>
-            <Title>소속팀</Title>
-            <Detail>롯데 프링글스</Detail>
-          </Content>
-        </InfoBox>
-      </InfoDiv>
+      <ProfileImg src={"assets/chanho.jpg"}></ProfileImg>
+      <div>
+        <div>포지션</div>
+        <div>포지션</div>
+        <div>포지션</div>
+        <div>포지션</div>
+        <div>포지션</div>
+      </div>
+      <Name>박찬호</Name>
+      <Name>박찬호</Name>
     </Wrapper>
   );
 };
