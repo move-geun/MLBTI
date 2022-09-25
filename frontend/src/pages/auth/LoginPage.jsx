ff5757a14c64fca380debe84d7dad941ed93daa5
이동근 <ehdrms1426@gmail.com>
2022-09-25 16:50:13 +0900

    add: 로그인 기능

--- frontend/src/pages/auth/LoginPage.jsx
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -1,22 +1,5 @@
 import { LoginBox, FlexRow, InputBox, FlexSpan } from "./LoginPage.style";
 
-// 로그인
-// export const login = createAsyncThunk(
-//   'LOGIN',
-//   async (userData:any, { rejectWithValue }) => {
-//     try {
-//       const res = await http.post('member/login.do', userData)
-//       const accessToken = res.data.accessToken
-//       const refreshToken = res.data.refreshToken
-//       window.localStorage.setItem('access-Token', accessToken);
-//       window.localStorage.setItem('refresh-Token', refreshToken);
-//       return res
-//     } catch (err:any) {
-//       return rejectWithValue(err.response) //err안에 response로 담겨있음
-//     }
-//   },
-// )
-
 const LoginPage = () => {
   return (
     <LoginBox>
