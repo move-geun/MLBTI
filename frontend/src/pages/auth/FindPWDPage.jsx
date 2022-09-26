a65c0ab9bcf837715a86a79f74b7269206ad6b8c
이동근 <ehdrms1426@gmail.com>
2022-09-26 16:15:56 +0900

    add:비밀번호 변경

--- frontend/src/pages/auth/FindPWDPage.jsx
+++ frontend/src/pages/auth/FindPWDPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { changePwd } from "./findpwd-slice";
+// import { useDispatch } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import { changePwd } from "./findpwd-slice";
 import {
   FindPWDBox,
   InputBox,
