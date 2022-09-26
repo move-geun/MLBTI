31fd34219d3a75831f1cb8684d5a25510623e00b
이동근 <ehdrms1426@gmail.com>
2022-09-26 17:17:44 +0900

    add: 비밀번호 찾기

--- frontend/src/pages/auth/FindPWDPage.jsx
+++ frontend/src/pages/auth/FindPWDPage.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-// import { useDispatch } from "react-redux";
-// import { useNavigate } from "react-router-dom";
-// import { changePwd } from "./findpwd-slice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { changePwd } from "./findpwd-slice";
+import { checkEmail, changwan } from "./signup-slice";
 import {
   FindPWDBox,
   InputBox,
@@ -10,10 +11,88 @@ import {
 } from "./FindPWDPage.style";
 
 const FindPWDPage = () => {
+  const dispatch = useDispatch();
+
+  // form에 입력한 정보
   const [userEmail, setUserEmail] = useState("");
   const [emailCert, setEmailCert] = useState("");
   const [userPwd, setUserPwd] = useState("");
   const [userPwdCheck, setUserPwdCheck] = useState("");
+
+  // 유효성 검사 변수
+  const [emailValid, setEmailValid] = useState(true);
+  const [pwdValid, setPwdValid] = useState(true);
+
+  // 이메일 인증 성공 여부
+  const [successCert, setSuccssCert] = useState(false);
+
+  // 비밀번호 재확인 여부 판별
+  const [checkedPwd, setCheckedPwd] = useState(false);
+
+  // 에러 메시지 방지 (입력이 들어오면 값 변경)
+  const [defaultEmail, setDefaultEmail] = useState(false);
+  const [defaultPwd, setDefaultPwd] = useState(false);
+
+  // 이메일 유효성 검사
+  const validateEmail = (e) => {
+    if (e.target.value) {
+      setDefaultEmail(true);
+    } else {
+      setDefaultEmail(false);
+    }
+
+    let regexp =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    if (regexp.test(e.target.value)) return setEmailValid(true);
+    else return setEmailValid(false);
+  };
+
+  // 비밀번호 유효성 검사 | 대소문자, 숫자 포함 9~16자
+  const validatePwd = (e) => {
+    var patternEngAtListOne = new RegExp(/[a-zA-Z]+/); // + for at least one
+    var patternNumAtListOne = new RegExp(/[0-9]+/); // + for at least one
+
+    if (e.target.value) {
+      setDefaultPwd(true);
+    } else {
+      setDefaultPwd(false);
+    }
+
+    if (
+      patternEngAtListOne.test(e.target.value) &&
+      patternNumAtListOne.test(e.target.value) &&
+      e.target.value.length >= 9 &&
+      e.target.value.length <= 16
+    ) {
+      return setPwdValid(true);
+    } else return setPwdValid(false);
+  };
+
+  // 비밀번호 재확인
+  const checkPassword = (e) => {
+    if (defaultPwd && pwdValid && e.target.value === userPwd) {
+      return setCheckedPwd(true);
+    } else return setCheckedPwd(false);
+  };
+
+  // 인증확인 메일 전송
+  function sendNumber() {
+    dispatch(checkEmail(userEmail));
+  }
+
+  // 인증 하기
+  async function checkCertNumber() {
+    console.log("이것도 되나");
+    const data = {
+      email: userEmail,
+      randomNumber: emailCert,
+    };
+    await dispatch(changwan(data))
+      .unwrap()
+      .then((res) => {
+        setSuccssCert(res);
+      });
+  }
 
   return (
     <FindPWDBox>
