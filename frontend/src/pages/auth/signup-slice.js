e5cc727eed6b44c65f25a525d8325c83138b59dd
배송윤 <nename8@naver.com>
2022-09-26 16:27:52 +0900

    feat: 회원가입 수정

--- frontend/src/pages/auth/signup-slice.js
+++ frontend/src/pages/auth/signup-slice.js
@@ -1,63 +1,90 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import axios from '../../api/http';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../api/http";
+
+// 회원가입
+export const signup = createAsyncThunk(
+  "SINGUP",
+  async (userData, { rejectWithValue }) => {
+    try {
+      console.log("회원가입 정보", userData);
+      const res = await axios.post("/user/signin", userData);
+      return res;
+    } catch (err) {
+      return rejectWithValue(err.response);
+    }
+  }
+);
 
 // 이메일 인증번호 전송
 export const checkEmail = createAsyncThunk(
-  'CHECK_EMAIL',
+  "CHECK_EMAIL",
   async (email, { rejectWithValue }) => {
     try {
-      console.log('실행되나')
       // query 로 보내기
-      const res = await axios.post('/user/mail/send/certify', null, { params: { email } })
-      return res
+      const res = await axios.post("/user/mail/send/certify", null, {
+        params: { email },
+      });
+      return res;
     } catch (err) {
-      return rejectWithValue(err.response)
+      return rejectWithValue(err.response);
     }
   }
-)
+);
 
 // 인증번호 확인 slice
-// export const checkNumber = createAsyncThunk(
-//   'CHECK_NUMBER',
-//   async (number, {rejectWithValue}) => {
-//     try {
-//       const res = await axios.post('/user/signin')
-//     }
-//   }
-// )
+export const changwan = createAsyncThunk(
+  "CHECK_NUMBER",
+  async (data, { rejectWithValue }) => {
+    try {
+      let result = false;
+
+      // const res = await axios.post('/user/mail/valid/check', null, {params: { email: email, randomNumber: `${randomNumber}` }})
+      const res = await axios.post("/user/mail/valid/check", null, {
+        params: { email: data.email, randomNumber: data.randomNumber },
+      });
+      console.log(res.status)
+      if (res.status === 200) {
+        result = true;
+      } else {
+        result = false;
+      }
+      return result;
+    } catch (err) {
+      return rejectWithValue(err.response);
+    }
+  }
+);
 
 // 팀 받아오는 slice (닉네임 중복 체크 할 용도)
 export const getUserList = createAsyncThunk(
-  'GET_USERLIST',
-  async (UserNick, {rejectWithValue}) => {
+  "GET_USERLIST",
+  async (UserNick, { rejectWithValue }) => {
     try {
-      let result = true
-      console.log('유저닉네임',UserNick)
-      const res = await axios.get('/user/list')
-      for (let idx = 0; idx < res.data.length; idx++){
-        if ( UserNick === res.data[idx].nickname ) {
-           result = false
-           break
-        }  
+      let result = true;
+      const res = await axios.get("/user/list");
+      for (let idx = 0; idx < res.data.length; idx++) {
+        if (UserNick === res.data[idx].nickname) {
+          result = false;
+          break;
+        }
       }
-      
-      return result
-    } catch(err) {
-      return rejectWithValue(err.response)
+
+      return result;
+    } catch (err) {
+      return rejectWithValue(err.response);
     }
   }
-)
+);
 
 const initialState = {
   isLoading: false,
-}
+};
 
 const signupSlice = createSlice({
-  name: 'signup',
+  name: "signup",
   initialState,
   reducers: {},
-  extraReducers: {
-  },
-})
+  extraReducers: {},
+});
 
-export default signupSlice.reducer
+export default signupSlice.reducer;
