850f63af41f26ea825baa66e7341edd87ca4999d
박찬호 <taurus42977@gmail.com>
2022-09-26 16:16:08 +0900

    feat: 시뮬레이션 더미 데이터 생성

--- backend/src/main/java/com/ssafy/api/controller/SimulController.java
+++ backend/src/main/java/com/ssafy/api/controller/SimulController.java
@@ -10,16 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.api.request.NoticeRegisterPostReq;
-import com.ssafy.common.model.response.BaseResponseBody;
-import com.ssafy.db.entity.Notices;
-import com.ssafy.db.entity.Users;
+import com.ssafy.db.entity.LiveGames;
+import com.ssafy.db.entity.SimulGameDatas;
+import com.ssafy.db.entity.SimulGameInning;
+import com.ssafy.db.entity.SimulGames;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 /**
 
   * @FileName : SimulController.java
@@ -43,10 +46,66 @@ public class SimulController {
         @ApiResponse(code = 404, message = "사용자 없음"),
         @ApiResponse(code = 500, message = "서버 오류")
     })
-	public ResponseEntity<? extends BaseResponseBody> register(
-			@RequestBody @ApiParam(value="경기 ", required = true) NoticeRegisterPostReq registerInfo) {
+	public ResponseEntity<String> register() {
 		
+		SimulGames res = new SimulGames();
+		res.setGamePk(123456);
+		res.setAwayName("한화 이글스");
+		res.setHomeName("기아 타이거즈");
+		res.setSeason(2022);
+		res.setVenueName("광주 구장");
+		res.setWeatherTemp(23);
+		res.setWeatherWind("24m/h");
+		SimulGameInning[] innings = res.getInngings();
+		for(int i=0; i<24; i++) {
+			innings[i] = new SimulGameInning();
+		}
+		SimulGameInning temp = res.getInngings()[0];
+		System.out.println(temp==null);
+		temp.setInning(1);
+		temp.setStatus("초");
+		SimulGameDatas e = new SimulGameDatas();
+		e.setBatSide("R");
+		e.setBatterName("노수광");
+		e.setEvent("1루타");
+		e.setFirstBase(false);
+		e.setSecondBase(false);
+		e.setThirdBase(false);
+		e.setIndex(0);
+		e.setInning(1);
+		e.setOutCount(0);
+		e.setPitcherName("김민수");
+		e.setRbi(0);
+		temp.getDatas().add(e);
 		
-		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+		e = new SimulGameDatas();
+		e.setBatSide("R");
+		e.setBatterName("김인환");
+		e.setEvent("삼진아웃");
+		e.setFirstBase(true);
+		e.setSecondBase(false);
+		e.setThirdBase(false);
+		e.setIndex(1);
+		e.setOutCount(0);
+		e.setPitcherName("김민수");
+		e.setRbi(0);
+		temp.getDatas().add(e);
+		
+		e = new SimulGameDatas();
+		e.setBatSide("R");
+		e.setBatterName("하주석");
+		e.setEvent("병살타");
+		e.setFirstBase(true);
+		e.setSecondBase(false);
+		e.setThirdBase(false);
+		e.setIndex(0);
+		e.setInning(1);
+		e.setOutCount(2);
+		e.setPitcherName("김민수");
+		e.setRbi(0);
+		temp.getDatas().add(e);
+		Gson gson = new Gson();
+		String json = gson.toJson(res);
+		return ResponseEntity.status(200).body(json);
 	}
 }
---
+++ backend/src/main/java/com/ssafy/db/entity/SimulGames.java
@@ -0,0 +1,28 @@
+package com.ssafy.db.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+
+* @FileName : LiveGames.java
+* @Date : 2022. 9. 22
+* @작성자 : 박찬호
+* @변경이력 : x
+* @프로그램 설명 : LiveGames 모델 정의
+*/
+@Getter
+@Setter
+public class SimulGames {
+	Integer gamePk;
+	Integer season;
+	String awayName;
+	String homeName;
+	String venueName;
+	Integer weatherTemp;
+	String weatherWind;
+	SimulGameInning[] inngings = new SimulGameInning[24];
+}
