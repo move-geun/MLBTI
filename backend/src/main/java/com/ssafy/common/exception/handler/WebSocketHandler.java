0dc4c8cd4f01227cc29008b67c06e0ff892a6aff
김동우 <sunbi5252@naver.com>
2022-09-28 10:24:36 +0900

    feat: 전체 조회 추가

--- backend/src/main/java/com/ssafy/common/exception/handler/WebSocketHandler.java
+++ backend/src/main/java/com/ssafy/common/exception/handler/WebSocketHandler.java
@@ -1,19 +1,28 @@
 package com.ssafy.common.exception.handler;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class WebSocketHandler extends TextWebSocketHandler{
 
     private static final ConcurrentHashMap<String, WebSocketSession> CLIENTS = new ConcurrentHashMap<String, WebSocketSession>();
 	
+    ObjectMapper json = new ObjectMapper();
+    
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         CLIENTS.put(session.getId(), session);
@@ -26,21 +35,34 @@ public class WebSocketHandler extends TextWebSocketHandler{
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-    	System.out.println("session is");
-    	System.out.println(session);
-    	System.out.println("message is");
-    	System.out.println(message);
-    	
-        String id = session.getId();  //메시지를 보낸 아이디
+
+    	Map<String, Object> dataMap = new HashMap<>();
+    	String payload = message.getPayload();
+        String senderId = (String) session.getAttributes().get("sessionId");  //메시지를 보낸 아이디
+        String id = session.getId();
+        System.out.println("payload: "+payload);
+        dataMap.put("message",payload);
+        dataMap.put("id",senderId);
+        
+     // Convert Map to byte array
+//        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+//        ObjectOutputStream out = new ObjectOutputStream(byteOut);
+//        out.writeObject(dataMap);
+        String msg = json.writeValueAsString(dataMap);
+
         CLIENTS.entrySet().forEach( arg->{
             if(!arg.getKey().equals(id)) {  //같은 아이디가 아니면 메시지를 전달합니다.
                 try {
-                    arg.getValue().sendMessage(message);
-                } catch (IOException e) {
+//                    arg.getValue().sendMessage(new TextMessage(byteOut.toByteArray()));
+                	arg.getValue().sendMessage(new TextMessage(msg));
+                    } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         });
+        
+        
+        
     }
     
 //    @Override
---
+++ backend/src/main/java/com/ssafy/db/dto/PlayersDto.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.ssafy.db.dto;
+
+import com.ssafy.db.entity.Batters;
+import com.ssafy.db.entity.Pitchers;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+
+  * @FileName : BattersDto.java
+  * @Project : mlb-analysis-project
+  * @Date : 2022. 9. 27 
+  * @작성자 : 김동우
+  * @변경이력 :
+  * @프로그램 설명 : custom team 꾸릴 때 만드는 dto
+  */
+
+@Getter
+@Setter
+public class PlayersDto {
+	String name;
+	int season;
+	String league;
+	String teamName;
+	String position;
+	float indicator;
+	
+	public static PlayersDto of(Batters batters) {
+		PlayersDto b = new PlayersDto();
+		b.setName(batters.getName());
+		b.setLeague(batters.getLeague());
+		b.setSeason(batters.getSeason());
+		b.setTeamName(batters.getTeamName());
+		// 여기서 타율 계산
+		float batAvg = (batters.getLeftHitNum()+batters.getRightHitNum()+batters.getLeftTwobHitNum()+batters.getRightTwobHitNum()+batters.getLeftThreebHitNum()+batters.getRightThreebHitNum()+batters.getLeftHrNum()+batters.getRightHrNum())/(float)(batters.getLeftAtBatNum()+batters.getRightAtBatNum());
+		b.setIndicator(batAvg);
+		return b;
+	}
+	
+	public static PlayersDto of(Pitchers pitchers) {
+		PlayersDto b = new PlayersDto();
+		b.setName(pitchers.getName());
+		b.setLeague(pitchers.getLeague());
+		b.setSeason(pitchers.getSeason());
+		b.setTeamName(pitchers.getTeamName());
+		// 여기서 타율 계산
+		float era = Math.round(((float)(pitchers.getLeftEr() + pitchers.getRightEr())*9*1000)/(pitchers.getLeftInningNum() + pitchers.getRightInningNum()))/(float)1000;
+		b.setIndicator(era);
+		return b;
+	}
+}
