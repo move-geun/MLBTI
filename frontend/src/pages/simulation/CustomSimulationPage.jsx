0a28528a85afabe262bc38d7ce1f4d603fb02bd7
배송윤 <nename8@naver.com>
2022-10-02 23:58:30 +0900

    feat: 시뮬레이션 모달 수정

--- frontend/src/pages/simulation/CustomSimulationPage.jsx
+++ frontend/src/pages/simulation/CustomSimulationPage.jsx
@@ -6,13 +6,22 @@ import {
   TeamContainer,
   TeamCase,
   ModalBox,
+  Header,
+  ListWrap,
 } from "./CustomSimulationPage.style";
-
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getNational, getAmerican } from "./customsimulation-slice";
 // 모달 연결
 // 승률 높은 곳에 색 변경
 // 수정필요
 
 const CustomSimulationPage = () => {
+  const dispatch = useDispatch();
   const [open, setOpen] = React.useState(false);
   const [opensec, setOpenSec] = React.useState(false);
   const handleOpen = () => setOpen(true);
@@ -21,11 +30,40 @@ const CustomSimulationPage = () => {
   const handleClosesec = () => setOpenSec(false);
   // const chat = () => console.log("눌러짐");
 
+  // 리그 선택 필터링
+  const [legueName, setLegueName] = React.useState("nationalMLB");
+
+  const [nationalList, setNationalList] = useState([]);
+  const [americanList, setAmericanList] = useState([]);
+
+  const handleChange = (event) => {
+    setLegueName(event.target.value);
+  };
+
+  useEffect(() => {
+    dispatch(getNational())
+      .unwrap()
+      .then((res) => {
+        setNationalList(res);
+      });
+    dispatch(getAmerican())
+      .unwrap()
+      .then((res) => {
+        setAmericanList(res);
+      });
+  }, []);
+
+  console.log(nationalList)
   return (
     <CustomConatiner>
+      <Header>매치업 설정하기</Header>
       <TeamContainer>
-        <TeamCase onClick={handleOpen}>
-          <img src="/assets/cap.png" alt="1팀이었던것.." />
+        <TeamCase>
+          <img
+            onClick={handleOpen}
+            src="/assets/teamlogo1.png"
+            alt="1팀이었던것.."
+          />
           <div>팀 설정하기</div>
           <div>56%</div>
           <Modal
@@ -37,7 +75,7 @@ const CustomSimulationPage = () => {
             <ModalBox>
               <div className="title">1팀 선택하기</div>
               <br />
-              <div className="content">
+              {/* <div className="content">
                 날씨
                 <div className="weather">
                   <input type="checkbox" />
@@ -55,7 +93,7 @@ const CustomSimulationPage = () => {
                   <input type="checkbox" />
                   <span>흘김</span>
                 </div>
-              </div>
+              </div> */}
               <br />
               <div className="team">
                 팀
@@ -64,14 +102,28 @@ const CustomSimulationPage = () => {
                   <button>검색하기</button>
                 </div>
                 <div className="filter">
-                  <input type="drop" placeholder="연도" />
-                  <input type="drop" placeholder="리그" />
-                  <input type="drop" placeholder="팀명" />
+                  <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="demo-simple-select-standard-label">
+                      리그 선택
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      value={legueName}
+                      onChange={handleChange}
+                      label="Age"
+                    >
+                      <MenuItem value={"nationalMLB"}>National League</MenuItem>
+                      <MenuItem value={"americanMLB"}>American League</MenuItem>
+                    </Select>
+                  </FormControl>
                 </div>
                 <div className="candidates">
-                  <div className="candi">초코맛팀</div>
-                  <div className="candi">딸기맛팀</div>
-                  <div className="candi">바나나맛팀</div>
+                  {nationalList.map((item) => (
+                    <ListWrap>
+                      <div className="candi">{item.name}</div>
+                    </ListWrap>
+                  ))}
                 </div>
               </div>
               <button className="change" onClick={handleClose}>
@@ -81,8 +133,12 @@ const CustomSimulationPage = () => {
           </Modal>
         </TeamCase>
         <span> VS </span>
-        <TeamCase onClick={handleOpensec}>
-          <img src="/assets/cap.png" alt="2팀이었던것.." />
+        <TeamCase>
+          <img
+            onClick={handleOpensec}
+            src="/assets/teamlogo2.png"
+            alt="2팀이었던것.."
+          />
           <div>팀 설정하기</div>
           <div>44%</div>
           <Modal
@@ -140,7 +196,7 @@ const CustomSimulationPage = () => {
       </TeamContainer>
       <Link to="/simulation" style={{ textDecoration: "none", color: "black" }}>
         <div className="start">
-          <img src="/assets/cap.png" alt="시작아이콘..이었던것" />
+          <img src="/assets/simulationStart.png" alt="시작아이콘..이었던것" />
           <span>시뮬레이션 시작</span>
         </div>
       </Link>
