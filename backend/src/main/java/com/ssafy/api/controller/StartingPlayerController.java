8af3ab7e1cf26ae65376a8fea8e8b02031b2cfc1
김동우 <sunbi5252@naver.com>
2022-10-03 18:55:43 +0900

    feat: 금일 경기 player uid 가져오기

--- backend/src/main/java/com/ssafy/api/controller/StartingPlayerController.java
+++ backend/src/main/java/com/ssafy/api/controller/StartingPlayerController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.api.service.BaseballPlayerService;
 import com.ssafy.api.service.ScheduleService;
+import com.ssafy.api.service.StartingPlayerService;
+import com.ssafy.db.dto.StartingPlayerDto;
 import com.ssafy.db.entity.BaseballPlayers;
 import com.ssafy.db.entity.Schedules;
 
@@ -35,10 +37,12 @@ import io.swagger.annotations.ApiResponses;
 @RestController
 @RequestMapping("/api/starting_player")
 public class StartingPlayerController {
-	@Autowired
-	BaseballPlayerService baseballPlayerService;
+	
 	@Autowired
 	ScheduleService scheduleService;
+	
+	@Autowired
+	StartingPlayerService startingPlayerService;
 	
 	@GetMapping("/")
 	@ApiOperation(value = "선수 상세 조회", notes = "<strong>uid</strong>를 통해 선수를 상세조회한다.") 
@@ -47,13 +51,16 @@ public class StartingPlayerController {
         @ApiResponse(code = 404, message = "선수 없음"),
         @ApiResponse(code = 500, message = "서버 오류")
     })
-	public ResponseEntity<BaseballPlayers> getStartingPlayerByDateAndTeamUid(
-			@ApiParam(value="team_uid", required = true) @RequestParam("team_uid") int team_uid,
-			@ApiParam(value="date", required = true) @RequestParam("date") String date
+	public ResponseEntity<List<StartingPlayerDto>> getStartingPlayerByDateAndTeamUid(
+			@ApiParam(value="120", required = true) @RequestParam("team_uid") int team_uid,
+			@ApiParam(value="20221003", required = true) @RequestParam("date") String date
 			) {
-		String processedDate = date.substring(0,4)+"-"+date.substring(4,6)+"-"+date.substring(6,8);
+		String processedDate = date.substring(6,8) + "/"+date.substring(4,6)+"/" + date.substring(0,4);
 		System.out.println(processedDate);
-		List<Schedules> s = (ArrayList<Schedules>) scheduleService.getScheduleByDate(processedDate);
+//		List<Schedules> s = (ArrayList<Schedules>) scheduleService.getScheduleByDate(processedDate);
+		String url = "https://statsapi.mlb.com/api/v1/teams/"+Integer.toString(team_uid)+"/roster?date="+processedDate;
+		List<StartingPlayerDto> l = startingPlayerService.getStartingPlayerByApi(url);
+		
 		/*
 		 * 1. schedule에서 그날 경기가 있는 team_id list 가져오기
 		   2. https://statsapi.mlb.com/api/v1/teams/143/roster?date=03/10/2022
@@ -61,9 +68,8 @@ public class StartingPlayerController {
 		   3. front 에서는 가져온 player_uid 리스트를 simulation에 보냄
 		   4. back은 player_uid 들을 가져와서 시뮬 돌림
 		 */
-		
-
-		return null;
+		System.out.println(url);
+		return ResponseEntity.status(200).body(l);
 	}
 	
 }
