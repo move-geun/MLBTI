bc525264038858fab79356d6da8fd4db9605ee83
배송윤 <nename8@naver.com>
2022-10-07 02:36:33 +0900

    fea: 내 프로필 추가

--- frontend/src/pages/simulation/CustomSimulationPage.jsx
+++ frontend/src/pages/simulation/CustomSimulationPage.jsx
@@ -39,11 +39,12 @@ const CustomSimulationPage = () => {
   // 선택한 팀 정보
   const [selectHome, setSelectHome] = useState([]);
   const [selectAway, setSelectAway] = useState([]);
-
-  // 내 정보 | 내 팀
+  
+  
+  // 내 정보 | 내 팀 
   const [myInfo, setMyInfo] = useState("");
   const [myTeam, setMyTeam] = useState();
-
+  
   // 내 팀 선택 시
   const [isClickMyTeam, setIsClickMyTeam] = useState(false);
 
@@ -106,10 +107,12 @@ const CustomSimulationPage = () => {
     setAmericanSearchList(value2);
   };
 
-  //  내 팀 선택 시
+
+  //  내 팀 선택 시 
   const clickMyTeam = () => {
     setIsClickMyTeam(true);
-  };
+
+  }
 
   useEffect(() => {
     dispatch(myprofile())
@@ -140,22 +143,22 @@ const CustomSimulationPage = () => {
       dispatch(getMyteam(data))
         .unwrap()
         .then((res) => {
-          setMyTeam(res.data[0].user);
+          setMyTeam(res[0]);
         });
     }
   }, [myInfo]);
 
+  
+
   // 팀 설정 안했을 시 페이지 이동 막기
   const isSelectedTeams = (event) => {
-    if (
-      (selectHome.length === 0 && !isClickMyTeam) ||
-      selectAway.length === 0
-    ) {
+    if ((selectHome.length === 0 && !isClickMyTeam) || selectAway.length === 0) {
       event.preventDefault();
       alert("두 팀 모두 선택해 주세요");
     }
   };
-
+  console.log(isClickMyTeam, selectHome, selectAway)
+  console.log(myTeam)
   return myTeam ? (
     <CustomConatiner>
       <Header>매치업 설정하기</Header>
@@ -163,16 +166,11 @@ const CustomSimulationPage = () => {
         <TeamCase>
           {isClickMyTeam ? (
             <div>
-              <img
-                alt="mylogo"
-                onClick={handleOpen}
-                className="myteamlogo"
-                src={"/assets/customTeamLogo.png"}
-              ></img>
-              <div>팀명: {myTeam.myTeamName}</div>
-              <div>구단주: {myTeam.nickname}</div>
+              <img alt="mylogo" onClick={handleOpen} className="myteamlogo" src={'/assets/customTeamLogo.png'}></img>
+              <div>팀명: {myTeam.user.myTeamName}</div>
+              <div>구단주: {myTeam.user.nickname}</div>
             </div>
-          ) : selectHome.length !== 0 ? (
+          ): selectHome.length !== 0 ? (
             <div>
               <img onClick={handleOpen} src={selectHome.logo} alt="선택한 팀" />
               <div>팀명: {selectHome.clubName}</div>
@@ -188,7 +186,8 @@ const CustomSimulationPage = () => {
               />
               <div>팀 설정하기</div>
             </div>
-          )}
+          )
+          }
           <Modal
             open={open}
             onClose={handleClose}
@@ -227,19 +226,15 @@ const CustomSimulationPage = () => {
                   </FormControl>
                 </div>
                 <div className="candidates">
-                  <div onClick={clickMyTeam} className="myteam">
-                    <img
-                      className="myteamlogo"
-                      src={"/assets/customTeamLogo.png"}
-                    ></img>
-                    {myTeam.myTeamName} (내 구단)
-                  </div>
+                  <div onClick={clickMyTeam}  className="myteam">
+                    <img className="myteamlogo" src={'/assets/customTeamLogo.png'}></img>{myTeam.user.myTeamName} (내 구단)
+                    </div>
                   {leagueName === "nationalMLB"
                     ? nationalSearchList.map((item, idx) => (
                         <ListWrap
                           value={idx}
                           onClick={(e) => {
-                            setIsClickMyTeam(false);
+                            setIsClickMyTeam(false)
                             setSelectHome(item);
                           }}
                         >
@@ -252,7 +247,7 @@ const CustomSimulationPage = () => {
                     : americanSearchList.map((item, idx) => (
                         <ListWrap
                           onClick={() => {
-                            setIsClickMyTeam(false);
+                            setIsClickMyTeam(false)
                             setSelectHome(item);
                           }}
                         >
@@ -363,15 +358,11 @@ const CustomSimulationPage = () => {
               </button>
             </ModalBox>
           </Modal>
-        </TeamCase>
+        </TeamCase> 
       </TeamContainer>
       <Link
         to={"/simulation"}
-        state={
-          isClickMyTeam
-            ? { home: myTeam.email, away: selectAway }
-            : { home: selectHome, away: selectAway }
-        }
+        state={ isClickMyTeam ? ({ home: myTeam.email, away: selectAway}) : {home: selectHome, away: selectAway} }
         style={{ textDecoration: "none", color: "black" }}
         onClick={isSelectedTeams}
       >
