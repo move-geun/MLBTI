26f6a26157eb159b93ca66e5930f005d63fc913e
이동근 <ehdrms1426@gmail.com>
2022-10-07 07:22:17 +0900

    add:css

--- frontend/src/components/simulation/SimulationResult.style.js
+++ frontend/src/components/simulation/SimulationResult.style.js
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 
+const simulDiv = styled.div`
+  width: 500px;
+  .simul_tab {
+    width: 500px;
+  }
+`;
+
 const TabMenu = styled.ul`
-
-
   display: flex;
   flex-direction: row;
   justify-items: center;
@@ -11,7 +16,7 @@ const TabMenu = styled.ul`
   margin-bottom: 2rem;
 
   .submenu {
-    width:100% auto;
+    width: 100% auto;
     padding: 15px 10px;
     cursor: pointer;
   }
@@ -23,11 +28,10 @@ const TabName = styled.span`
   border-color: inherit;
   font-size: 1rem;
   border-radius: 30%;
-`
+`;
 const StatusContainer = styled.div`
-  padding-left: 4rem;
-`
-
+  /* padding-left: 4rem; */
+`;
 
 const StatusBtn = styled.span`
   /* background-color: darkgray;
@@ -37,40 +41,44 @@ const StatusBtn = styled.span`
   padding: 0.5rem; */
 
   float: left;
-  padding: 5px 10px;
   border-radius: 15px;
   /* font-family: "paybooc-Light", sans-serif; */
-  margin: 3px;
   text-decoration: none;
   transition: 0.25s;
 
   /* border: 3px solid #6aafe6; */
   color: #6e6e6e;
-
-`
-
+`;
 
 const BatterContatiner = styled.div`
-
-padding: 3rem;
-/* display: flex; */
+  padding: 3rem;
+  /* display: flex; */
   /* justify-content:center;  */
-  align-items : center;
+  align-items: center;
   /* background-color: green; */
-`
+`;
 const BatterResult = styled.div`
-  
-  margin: 1em;
-  margin-left: 2%;
+  margin: 1em auto;
   width: 20rem;
   border-radius: 0.7em;
   border-style: none;
   box-shadow: 5px 5px 10px 0.1px gray;
   padding: 1em;
-  background-color: #f2dd8a;
+  background-color: black;
+  color: white;
+  text-align: center;
 
-`
-export {TabMenu, TabName, StatusContainer, StatusBtn, BatterContatiner, BatterResult };
-
-
-
+  :nth-child(even) {
+    background-color: gray;
+    color: black;
+  }
+`;
+export {
+  simulDiv,
+  TabMenu,
+  TabName,
+  StatusContainer,
+  StatusBtn,
+  BatterContatiner,
+  BatterResult,
+};
